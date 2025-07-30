af78c432f52f4a7db6aeed619fd98fbc73a8fa2b
ToshihiroOgino <tonigo0725@gmail.com>
2025-07-30 22:45:10 +0900

    Refactor lwIP options and enhance TCP connection handling in pico2-server

--- src/pico2-server.cpp
+++ src/pico2-server.cpp
@@ -1,10 +1,13 @@
-#include "lwip/apps/httpd.h"
-#include "lwip/apps/mdns.h"
-#include "lwip/init.h"
-#include "lwip/ip4_addr.h"
+#include <time.h>
+
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
-#include <stdio.h>
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+
+// #include <cstring>
+// #include <stdio.h>
 
 #include "error_handler.h"
 #include "xip_config.h"
@@ -16,6 +19,28 @@ void toggle_led() {
 	cyw43_led = !cyw43_led;
 	printf("LED State: %s\n", cyw43_led ? "ON" : "OFF");
 	cyw43_arch_gpio_put(BUILTIN_LED, cyw43_led);
+}
+
+static bool connected = false;
+
+err_t connected_fn(void *arg, struct tcp_pcb *tpcb, err_t err) {
+	if (err != ERR_OK) {
+		auto msg = "Connection failed: " + std::to_string(err);
+		return err;
+	}
+	printf("Connected to server successfully!\n");
+	const char *msg = "Hello from Pico!";
+	size_t msg_len = strlen(msg);
+	struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, msg_len, PBUF_RAM);
+	if (!p) {
+		handle_error("Failed to allocate pbuf");
+		return ERR_MEM;
+	}
+	connected = true;
+	memcpy(p->payload, msg, msg_len);
+	tcp_write(tpcb, p->payload, p->len, TCP_WRITE_FLAG_COPY);
+	tcp_output(tpcb);
+	return ERR_OK;
 }
 
 int main() {
@@ -40,8 +65,22 @@ int main() {
 		return 1;
 	}
 
+	const auto ip = "192.168.1.52";
+	auto pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
+	if (!pcb) {
+		handle_error("Failed to create TCP PCB");
+		return 1;
+	}
+
+	cyw43_arch_lwip_begin();
+	ip_addr_t dest;
+	ip4addr_aton(ip, &dest);
+	auto err = tcp_connect(pcb, &dest, 8000, connected_fn);
+
 	while (true) {
 		toggle_led();
 		sleep_ms(1000);
 	}
+
+	cyw43_arch_lwip_end();
 }
